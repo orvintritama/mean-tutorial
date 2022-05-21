fa7e646a5b25b1eab0926be34ccdff0ccfe1c8f4
orvinrfc <orvinrfc@hotmail.com>
2022-05-20 17:02:23 -0700

    rebuild code due to node errors

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,27 +1,14 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostCreateComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
