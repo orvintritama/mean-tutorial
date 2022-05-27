57838735ccc91340b7994a5e45c7f2011af01c63
orvinrfc <orvinrfc@hotmail.com>
2022-05-26 17:52:50 -0700

    switched eventemitter to rxjs subscription

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
-import { Post } from '../post.model'
+import { PostsService } from "../posts.service";
 @Component({
     selector: 'app-post-create',
     templateUrl: './post-create.component.html',
@@ -11,13 +11,13 @@ import { Post } from '../post.model'
 export class PostCreateComponent {
     enteredTitle = '';
     enteredContent = '';
-    @Output() postCreated = new EventEmitter();
+
+    constructor(public postsService: PostsService) {}
     
     onAddPost(form: NgForm) {
         if (form.invalid) {
             return;
         }
-        const post: Post = { title: form.value.title, content: form.value.content };
-        this.postCreated.emit(post);
+        this.postsService.addPost(form.value.title, form.value.content);
     }
 }
